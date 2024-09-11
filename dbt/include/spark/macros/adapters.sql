9f1a03093dfd0839ebea5b81d0e9f13b224de806
authentic7 <Duman.makhanbetov@gmail.com>
2024-09-12 01:09:17 +0500

    Update adapters.sql

--- dbt/include/spark/macros/adapters.sql
+++ dbt/include/spark/macros/adapters.sql
@@ -138,7 +138,7 @@
 
 {#-- We can't use temporary tables with `create ... as ()` syntax --#}
 {% macro spark__create_temporary_view(relation, compiled_code) -%}
-    create or replace temporary view {{ relation }} as
+    create or replace table {{ relation }}  as
       {{ compiled_code }}
 {%- endmacro -%}
 
